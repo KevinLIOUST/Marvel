4734c89f0c17bdd08a51b0279808b006e72ff129
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-02 14:06:31 +0200

    Affichage Terminé ! :)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Marvel
+
+Ce mini projet est un entrainement en algorithmique pour garder le rythme !!!! Elle est bonne celle-là !!!!
+C'est pour faire un affichage de super-héros de MARVEL avec l'aide d'un fichier JSON !!!!
+Fait par Kein LIOUST DIT LAFLEUR !!!!
--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
 // Fonction pour afficher les 10 premiers super-héros de Marvel avec l'aide du fichier JSON
 function afficherSuperHeros() {
+    fetch("js/script.json")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
+            data.forEach(hero => {
+                document.getElementById("super-heros").innerHTML +=
+                    `<div class="fiche-super-heros rounded-3 m-4 w-25">
+                    <div class="d-flex justify-content-center">
+                        <img src="data:image/png;base64, ${hero.image}" alt="Super-Héros 1">
+                    </div>
+                    <p class="text-white m-3"><b>Nom : </b>${hero.name}</p>
+                    <p class="text-white m-3"><b>Vrai nom : </b>${hero.realName}</p>
+                    <p class="text-white m-3"><b>Super-pouvoirs :</b>${hero.powers}</p>
+                    <ul class="list m-3">
+                </ul>
+                    <p class="text-white m-3"><b>Ville : </b> ${hero.city}</p>
+                    <p class="text-white m-3"><b>Date d'apparition : </b>${hero.firstAppearance}</p>
+                </div>
+                `;
+            });
+        })
 }
+
+// Fonction pour gérer l'affichage des super-pouvoirs
+function affichageSuperPouvoirs() {
+    fetch("js/script.json")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(hero => {
+                for (let i = 0; i < hero.length; i++) {
+                    for (let j = 0; j < hero.powers.length; j++) {
+                        document.getElementById("super-heros").innerHTML += `
+                <li class="list-item text-white">${hero[i].powers[j]}</li>
+                `;
+                    }
+                }
+            })
+        })
+}
+afficherSuperHeros();
